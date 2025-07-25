f06d7c0a4974d41322b429c2bb5be5fd6893c5b4
Prasad8099 <mnsv009@gmail.com>
2025-07-25 08:22:34 +0530

    Update output.tf

--- modules/EC2/output.tf
+++ modules/EC2/output.tf
@@ -1,1 +1,28 @@
+variable "region" {
+  default = "ap-south-1"
+}
+
+variable "environment" {
+  default = "dev"
+}
+
+variable "vpc_name" {}
+variable "vpc_cidr" {}
+variable "azs" {
+  type = list(string)
+}
+variable "public_subnets" {
+  type = list(string)
+}
+variable "private_subnets" {
+  type = list(string)
+}
+
+# EC2 variables
+variable "ec2_name" {}
+variable "ec2_ami" {}
+variable "ec2_instance_type" {
+  default = "t3.micro"
+}
+variable "ec2_key_name" {}
 
